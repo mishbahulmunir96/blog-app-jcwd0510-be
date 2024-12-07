00935aa63d69e363e4b57060f550ba6263bd1ec6
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-12-07 07:17:34 +0700

    feat: register

---
+++ src/services/auth/register.service.ts
@@ -0,0 +1,29 @@
+import { User } from "@prisma/client";
+import { prisma } from "../../lib/prisma";
+import { hashPassword } from "../../lib/argon";
+
+export const registerService = async (body: User) => {
+  try {
+    const { name, email, password } = body;
+
+    const existingUser = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new Error("Email already exist");
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    return await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+};
