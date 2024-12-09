24be850cfc7109a8b5c449c20ca765586063421c
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-12-09 11:05:11 +0700

    feat: login

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -17,3 +17,19 @@ export const validateRegister = [
     next();
   },
 ];
+
+export const validateLogin = [
+  body("email").notEmpty().withMessage("Email is required").isEmail(),
+  body("password").notEmpty().withMessage("Password is required"),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).send({ message: errors.array()[0].msg });
+      return;
+    }
+
+    next();
+  },
+];
