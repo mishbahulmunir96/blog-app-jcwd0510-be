4e628574ec6cc4bb6ff8c63184218b8ec0183f57
mishbahulmunir96 <157870568+mishbahulmunir96@users.noreply.github.com>
2024-12-09 15:36:33 +0700

    feat: forgot password (#3)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { registerService } from "../services/auth/register.service";
 import { loginService } from "../services/auth/login.service";
+import { forgotPasswordService } from "../services/auth/forgot-password.service";
 
 export const registerController = async (
   req: Request,
@@ -21,6 +22,18 @@ export const loginController = async (
 ) => {
   try {
     const result = await loginService(req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+export const forgotPasswordController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await forgotPasswordService(req.body);
     res.status(200).send(result);
   } catch (error) {
     next(error);
---
+++ src/services/auth/forgot-password.service.ts
@@ -0,0 +1,35 @@
+import { User } from "@prisma/client";
+import { sign } from "jsonwebtoken";
+import { BASE_URL_FRONTEND, JWT_SECRET_FORGOT_PASSWORD } from "../../config";
+import { prisma } from "../../lib/prisma";
+import { transporter } from "../../lib/nodemailer";
+
+export const forgotPasswordService = async (body: Pick<User, "email">) => {
+  try {
+    const { email } = body;
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error("Invalid email address");
+    }
+
+    const token = sign({ id: user.id }, JWT_SECRET_FORGOT_PASSWORD!, {
+      expiresIn: "15m",
+    });
+
+    const link = `${BASE_URL_FRONTEND}/reset-password/${token}`;
+
+    transporter.sendMail({
+      to: email,
+      subject: "Link Reset Password",
+      html: `<a href="${link}" target="_blank">Reset Password here</a>`,
+    });
+
+    return { message: "seng email success" };
+  } catch (error) {
+    throw error;
+  }
+};
