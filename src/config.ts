70d1597e572f565815a94e8a049d4c45923403c7
mishbahulmunir96 <157870568+mishbahulmunir96@users.noreply.github.com>
2024-12-16 09:42:56 +0700

    feat: create blog (#7)

--- src/config.ts
+++ src/config.ts
@@ -9,3 +9,7 @@ export const JWT_SECRET_FORGOT_PASSWORD =
 export const GMAIL_EMAIL = process.env.GMAIL_EMAIL;
 export const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD;
 export const BASE_URL_FRONTEND = process.env.BASE_URL_FRONTEND;
+
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
--- src/routes/blog.router.ts
+++ src/routes/blog.router.ts
@@ -1,8 +1,23 @@
 import { Router } from "express";
-import { getBlogsController } from "../controllers/blog.controller";
+import {
+  createBlogsController,
+  getBlogsController,
+} from "../controllers/blog.controller";
+import { uploader } from "../lib/multer";
+import { fileFilter } from "../lib/fileFilter";
+import { validateCreateBlog } from "../validators/blog.validator";
+import { verifyToken } from "../lib/jwt";
 
 const router = Router();
 
 router.get("/", getBlogsController);
+router.post(
+  "/",
+  verifyToken,
+  uploader().fields([{ name: "thumbnail", maxCount: 1 }]),
+  fileFilter,
+  validateCreateBlog,
+  createBlogsController
+);
 
 export default router;
